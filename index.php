d6b4e544d6adf226a567828ef55617c07c966092
nyashka <oleshakirsanova@gmail.com>
2025-10-06 14:39:21 +0200

    adding-styles

--- index.php
+++ index.php
@@ -6,36 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-    <h1>Задача №1</h1>
-    <h3>Вариант 14, высокий уровень (1.2)</h3>
-    <form action="display.php" method="post">
-        <p>Введите площадь:<label>
-                <input type="number" name="sq"/>
-            </label>
-            <input type="submit" value="Отправить">
-    </form>
-    <form action="secondDisplay.php" method="post">
-        <h1>Задача №2</h1>
-        <h3>Вариант 16, высокий уровень (2.2)</h3>
-        <p>Введите число:<label>
-                <input type="number" name="mas1"/>
-            </label>
-            <input type="submit" value="Отправить">
-        </p>
-    </form>
-    <form action="thirdDisplay.php" method="post">
-        <h1>Задача №3</h1>
-        <h3>Вариант 20, высокий уровень (3.2)</h3>
-        <p>Введите число n: (>9999)<label>
-                <input type="number" name="num1"/>
-            </label>
-        <p>Введите число m:<label>
-                <input type="number" name="num2"/>
-            </label>
-            <input type="submit" value="Отправить">
-        </p>
-    </form>
+    <a href="first.php">Лабораторная 1</a> <br>
+    <a href="second.php">Лабораторная 2</a>
 </body>
 </html>
